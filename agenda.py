1b6cf49d5d431acd7a9b1607f507b4372892f303
Hector Ramos Gomez (ext) <hector.ramos.external@eviden.com>
2025-05-20 10:27:28 +0100

    Primer Commit

---
+++ agenda.py
@@ -0,0 +1,38 @@
+#Main de la agenda
+
+import funciones
+
+print(funciones.menuAgendaContactos())
+opcion = int(input("> "))
+
+while opcion != 11:
+    match opcion:
+        case 1: 
+            funciones.crearAgenda()
+        case 2:
+            funciones.mostrarAgenda()
+        case 3:
+            funciones.agregarContacto()
+        case 4:
+            funciones.buscarContacto()
+        case 5:
+            funciones.editarContacto()
+        case 6:
+            funciones.borrarContactos()
+        case 7:
+            funciones.importarArchivos()
+        case 8:
+            funciones.exportarArchivos()
+        case 9:
+            funciones.ordenarArchivo()
+        case _: 
+            print("Opción no válida. Intenta de nuevo.")
+
+    print(funciones.menuAgendaContactos())
+    try:
+        opcion = int(input("> "))
+    except ValueError:
+        print("Entrada no válida. Saliendo del programa.")
+        break
+
+print("Programa finalizado.")
---
+++ funciones.py
@@ -0,0 +1,151 @@
+#Funciones de la agenda
+
+def menuAgendaContactos():
+    print("Agenda ")
+    print("(1). Crea una agenda. ")
+    print("(2). Mostrar agenda. ")
+    print("(3). Agregar un contacto. ")
+    print("(4). Buscar contacto por nombre. ")
+    print("(5). Editar contacto. ")
+    print("(6). Eliminar contacto. ")
+    print("(7). Importar de un archivo a otro. ")
+    print("(8). Exportar a otro archvo. ")
+    print("(9). Datos ordenados alfabeticamente. ")
+
+
+# 1 Funcion de crear Agenda
+def crearAgenda () :
+    agenda = open ("agendaContactos.txt", "w")
+    agenda.close()
+
+# 2 Funcion agregar contactos varios
+def agregarContacto () :
+    nombre = input("Introduce tu nombre: ")
+    telefono = input("Introudce tu telfono: ")
+    correo = input("Introduce tu correo: ")
+    nombre = nombre
+
+    contacto_encontrado = False
+
+    with open("agendaContactos.txt", "r") as agenda:
+        lineas = agenda.readlines()
+
+    with open("agendaContactos.txt", "w") as agenda:
+        for linea in lineas:
+            if nombre not in linea:
+                agenda.write(linea)
+            else:
+                contacto_encontrado = True
+
+    if contacto_encontrado:
+        print("El contacto ha sido eliminado.")
+
+    with open("agendaContactos.txt", "a") as agenda:
+        agenda.write(f"Nombre: {nombre}, Telefono: {telefono}, Correo:{correo}\n")
+
+    print("El contacto se agrego correctamente")
+
+# 3 MOstra agendas
+def mostrarAgenda():
+    try:
+        with open("agendaContactos.txt", "r") as agenda:
+            content = agenda.read()
+            print(content)
+    except FileNotFoundError:
+        print("El archivo 'agendaContactos.txt' no existe.")
+
+#Buscador de contactos
+def buscarContacto():
+    nombre = input("El nombre es: ")
+
+    with open("agendaContactos.txt", "r") as agenda:
+        linea = agenda.readlines()
+        for row in linea:
+            palabra = nombre
+            if row.find(palabra) != -1:
+                print(f"El contacto {palabra} existe")
+
+
+#Editor de contactos
+def editarContacto():
+    nombre = input("El nombre del contacto existente es: ")
+    nuevoNombre = input("Introduce el nuevo nombre: ")
+    nuevoTelefono = input("Introduce el nuevo teléfono: ")
+    nuevoCorreo = input("Introduce el nuevo correo: ")
+
+    with open("agendaContactos.txt", "r") as agenda:
+        lineas = agenda.readlines()
+
+    contacto_encontrado = False
+
+
+    with open("agendaContactos.txt", "w") as agenda:
+        for linea in lineas:
+            if nombre in linea:
+                agenda.write(f"{nuevoNombre},{nuevoTelefono},{nuevoCorreo}\n")
+                contacto_encontrado = True
+            else:
+                agenda.write(linea)
+
+    if contacto_encontrado:
+        print("El contacto ha sido actualizado")
+    else:
+        print("No se encontró el contacto para actualizarlo")
+
+#Borrador de contactos 
+def borrarContactos():
+    nombre = input("Nombre del contacto a borrar: ")
+    contacto_encontrado = False
+
+    with open("agendaContactos.txt", "r") as agenda:
+        lineas = agenda.readlines()
+
+    with open("agendaContactos.txt", "w") as agenda:
+        for linea in lineas:
+            if nombre not in linea:
+                agenda.write(linea)
+            else:
+                contacto_encontrado = True
+
+    if contacto_encontrado:
+        print("El contacto ha sido eliminado.")
+    else:
+        print("Contacto no encontrado.")
+
+#Ordenador los datos
+
+def ordenarArchivo():
+    with open("agendaContactos.txt", "r") as archivo:
+        lineas = archivo.readlines()
+
+    lineas_ordenadas = sorted([linea.strip() for linea in lineas])
+
+    with open("agendaContactos.txt", "w") as archivo:
+        for linea in lineas_ordenadas:
+            archivo.write(linea + "\n")
+
+    print("Archivo ordenado alfabéticamente.")
+
+    with open("agendaContactos.txt", "r") as agenda:
+        content = agenda.read()
+        print(content)
+    
+
+#importar datos
+def importarArchivos():
+    importFile = input("Introduce el nombre del archivo a importar: ")
+    with open(importFile,'r') as firstfile, open('agendaContactos.txt','a') as secondfile:
+        for line in firstfile:
+             secondfile.write(line)
+
+#Exportar datos
+def exportarArchivos () :
+    exportFile = input("Introduce el nombre del archivo que quieres crear para exportar: ")
+    agenda = open (exportFile, "w")
+    agenda.close()
+
+    with open("agendaContactos.txt",'r') as firstfile, open(exportFile,'w') as secondfile:
+        for line in firstfile:
+            secondfile.write(line)
+
+#Validar datos
